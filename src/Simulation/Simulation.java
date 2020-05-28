3220378eda821d20e835d8be67f34152712e43c4
oy <roy.legziel@gmail.com>
2020-05-28 09:09:48 +0200

    working roasters(4real), agents are reset and reinitialized at every roaster changed. pls look at the method at Simulation to see.

--- src/Simulation/Simulation.java
+++ src/Simulation/Simulation.java
@@ -11,12 +11,12 @@ import java.util.ArrayList;
 
 public class Simulation {
 
-    public CEventList list;
-    public Queue consumer;
-    public Queue corp;
-    public Source sourcCon;
-    public Source sourcCorporate;
-    public Sink sink;
+    public static CEventList list;
+    public static Queue consumer;
+    public static Queue corp;
+    public static Source sourcCon;
+    public static Source sourcCorporate;
+    public static Sink sink;
 
     public static CSACorporate corpAgent1;
     public static CSACorporate corpAgent2;
@@ -25,12 +25,12 @@ public class Simulation {
     public static CSACorporate corpAgent5;
     public static CSACorporate corpAgent6;
 
-    public static CSAConsumer  consAgent1;
-    public static CSAConsumer  consAgent2;
-    public static CSAConsumer  consAgent3;
-    public static CSAConsumer  consAgent4;
-    public static CSAConsumer  consAgent5;
-    public static CSAConsumer  consAgent6;
+    public static CSAConsumer consAgent1;
+    public static CSAConsumer consAgent2;
+    public static CSAConsumer consAgent3;
+    public static CSAConsumer consAgent4;
+    public static CSAConsumer consAgent5;
+    public static CSAConsumer consAgent6;
 
 
     public Simulation(){
@@ -40,22 +40,49 @@ public class Simulation {
         this.sourcCon = new SourceConsumer(consumer, list, "Source consumer");
         this.sourcCorporate = new SourceCorporate(corp, list, "Source corporate");
         this.sink = new Sink("Sink 1");
-        this.corpAgent1 = new CSACorporate(list, corp, consumer, sink, "Corp1 agent");
-        this.corpAgent2 = new CSACorporate(list, corp, consumer, sink, "Corp2 agent");
-        this.corpAgent3 = new CSACorporate(list, corp, consumer, sink, "Corp3 agent");
-        this.corpAgent4 = new CSACorporate(list, corp, consumer, sink, "Corp4 agent");
-        this.corpAgent5 = new CSACorporate(list, corp, consumer, sink, "Corp5 agent");
-        this.corpAgent6 = new CSACorporate(list, corp, consumer, sink, "Corp6 agent");
+        // starting agents:
+        this.corpAgent1 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 1 agent");
+        this.corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 2 agent");
+        this.corpAgent3 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 3 agent");
+        this.corpAgent4 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 4 agent");
+        this.corpAgent5 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 5 agent");
+        this.corpAgent6 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 6 agent");
 
-        this.consAgent1 = new CSAConsumer(list, consumer, sink, "consumer1 agent");
-        this.consAgent2 = new CSAConsumer(list, consumer, sink, "consumer2 agent");
-        this.consAgent3 = new CSAConsumer(list, consumer, sink, "consumer3 agent");
-        this.consAgent4 = new CSAConsumer(list, consumer, sink, "consumer4 agent");
-        this.consAgent5 = new CSAConsumer(list, consumer, sink, "consumer5 agent");
-        this.consAgent6 = new CSAConsumer(list, consumer, sink, "consumer6 agent");
+
+        this.consAgent1 = new CSAConsumer(list, consumer, sink, "shift 1 cons 1 agent");
+        this.consAgent2 = new CSAConsumer(list, consumer, sink, "shift 1 cons 2 agent");
+        this.consAgent3 = new CSAConsumer(list, consumer, sink, "shift 1 cons 3 agent");
+        this.consAgent4 = new CSAConsumer(list, consumer, sink, "shift 1 cons 4 agent");
+        this.consAgent5 = new CSAConsumer(list, consumer, sink, "shift 1 cons 5 agent");
+        this.consAgent6 = new CSAConsumer(list, consumer, sink, "shift 1 cons 6 agent");
+
+
+
 
 
     }
+
+    public CEventList getList() {
+        return list;
+    }
+
+    public static void resetAgents() {
+        corpAgent1.setActive(false);
+        corpAgent2.setActive(false);
+        corpAgent3.setActive(false);
+        corpAgent4.setActive(false);
+        corpAgent5.setActive(false);
+        corpAgent6.setActive(false);
+
+        consAgent1.setActive(false);
+        consAgent2.setActive(false);
+        consAgent3.setActive(false);
+        consAgent4.setActive(false);
+        consAgent5.setActive(false);
+        consAgent6.setActive(false);
+    }
+
+
 	
 
         /**
@@ -85,54 +112,41 @@ public class Simulation {
 
     public static void rosterChange(int i){
         if(i==0){
-            corpAgent1.setActive(true);
-            corpAgent2.setActive(true);
-            corpAgent3.setActive(true);
-            corpAgent4.setActive(false);
-            corpAgent5.setActive(false);
-            corpAgent6.setActive(false);
+            resetAgents();
+            corpAgent1 = new CSACorporate(list, consumer, corp, sink, "shift 2 corp agent 1");
+            corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 2 corp agent 2");
+            corpAgent3 = new CSACorporate(list, consumer, corp, sink, "shift 2 corp agent 3");
 
-            consAgent1.setActive(true);
-            consAgent2.setActive(true);
-            consAgent3.setActive(true);
-            consAgent4.setActive(false);
-            consAgent5.setActive(false);
-            consAgent6.setActive(false);
+            consAgent1 = new CSAConsumer(list, consumer, sink, " shift 2 cons agent 1");
+            consAgent2 = new CSAConsumer(list, consumer, sink, " shift 2 cons agent 2");
+            consAgent3 = new CSAConsumer(list, consumer, sink, " shift 2 cons agent 3");
+
         }else if(i==1){
-            corpAgent1.setActive(true);
-            corpAgent2.setActive(true);
-            corpAgent3.setActive(false);
-            corpAgent4.setActive(false);
-            corpAgent5.setActive(false);
-            corpAgent6.setActive(false);
+            resetAgents();
+            corpAgent1 = new CSACorporate(list, consumer, corp, sink, "shift 3 corp agent 1");
+            corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 3 corp agent 2");
+            corpAgent3 = new CSACorporate(list, consumer, corp, sink, "shift 3 corp agent 3");
 
-            consAgent1.setActive(true);
-            consAgent2.setActive(true);
-            consAgent3.setActive(true);
-            consAgent4.setActive(true);
-            consAgent5.setActive(false);
-            consAgent6.setActive(false);
+            consAgent1 = new CSAConsumer(list, consumer, sink, " shift 3 cons agent 1");
+            consAgent2 = new CSAConsumer(list, consumer, sink, " shift 3 cons agent 2");
+            consAgent3 = new CSAConsumer(list, consumer, sink, " shift 3 cons agent 3");
+
         }else if(i==2){
-            corpAgent1.setActive(true);
-            corpAgent2.setActive(false);
-            corpAgent3.setActive(false);
-            corpAgent4.setActive(false);
-            corpAgent5.setActive(false);
-            corpAgent6.setActive(false);
+            resetAgents();
+            corpAgent1 = new CSACorporate(list, consumer, corp, sink, "shift 4 corp agent 1");
+            corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 4 corp agent 2");
+            corpAgent3 = new CSACorporate(list, consumer, corp, sink, "shift 4 corp agent 3");
 
-            consAgent1.setActive(true);
-            consAgent2.setActive(false);
-            consAgent3.setActive(false);
-            consAgent4.setActive(false);
-            consAgent5.setActive(false);
-            consAgent6.setActive(false);
+            consAgent1 = new CSAConsumer(list, consumer, sink, " shift 4 cons agent 1");
+            consAgent2 = new CSAConsumer(list, consumer, sink, " shift 4 cons agent 2");
+            consAgent3 = new CSAConsumer(list, consumer, sink, " shift 4 cons agent 3");
+
 
 
         }
+
+
     }
-
-
-
 
 
 }
