d611fad5e4f74638bb2892d998bcadcbfd0b7487
IgaSkorupska <43904884+IgaSkorupska@users.noreply.github.com>
2020-05-29 12:52:44 +0200

    Add files via upload

--- MATLAB/dataanalysis.m
+++ MATLAB/dataanalysis.m
@@ -1,18 +1,18 @@
 %% Reading Files
 
-customers = readmatrix('consumers1.csv');
+customers = readmatrix('consumers3.csv');
 [m_cus, n] = size(customers);
 customers = reshape(customers, [n, m_cus]);
 
-company = readmatrix('corporate1.csv');
+company = readmatrix('corporate3.csv');
 [m_com, n] = size(company);
 company = reshape(company, [n, m_com]);
 
-customer_arr = readmatrix('consumers_arr1.csv');
+customer_arr = readmatrix('consumers_arr3.csv');
 [m,n] = size(customer_arr);
 customer_arr = reshape(customer_arr, [n,m]);
 
-company_arr = readmatrix('corporate_arr1.csv');
+company_arr = readmatrix('corporate_arr3.csv');
 [m,n] = size(company_arr);
 company_arr = reshape(company_arr, [n,m]);
 
@@ -34,7 +34,7 @@ company_b = reshape(company_b, [n, m_comb]);
 
 
 %% Analysis of Customers data
-mean_cons = mean(customers);
+mean_cons = mean(customers)
 %% Plot
 % plot(customer_arr, customers)
 %% Confidence intervals
@@ -42,28 +42,28 @@ mean_cons = mean(customers);
 [conf90_a, conf90_b] = confintervals(customers, 0.1);
 exceeding = customers(customers>5);
 [m, n_ex] = size(exceeding);
-perc_5 = 100.*n_ex./m_cus;
+perc_5 = 100.*n_ex./m_cus
 % At least 95% in 10 minutes
-[conf95_a, conf95_b] = confintervals(customers, 0.05);
+[conf95_a, conf95_b] = confintervals(customers, 0.05)
 exceeding = customers(customers>10);
 [m, n_ex] = size(exceeding);
-perc_10 = 100.*n_ex./m_cus;
+perc_10 = 100.*n_ex./m_cus
 %% Unpaired t-test
 [t,p,r] = unpairedttest(customers_a, customers_b);
 %% Analysis of Company data
-mean_comp = mean(company);
+mean_comp = mean(company)
 %% Plot
 % plot(company_arr, company)
 %% Confidence intervals
 % At least 95% in 3 minutes
-[comp95_a, comp95_b] = confintervals(company, 0.05);
+[comp95_a, comp95_b] = confintervals(company, 0.05)
 exceeding = company(company>3);
 [m, n_ex] = size(exceeding);
-perc_3 = 100.*n_ex./m_com;
+perc_3 = 100.*n_ex./m_com
 % At least 99% in 7 minutes
 [comp99_a, comp99_b] = confintervals(company, 0.01);
 exceeding = company(company>7);
 [m, n_ex] = size(exceeding);
-perc_7 = 100.*n_ex./m_com;
+perc_7 = 100.*n_ex./m_com
 %% Unpaired t-test
 [t,p,r] = unpairedttest(company_a, company_b);
