27b0781b8a8fff7563901aac4dec9d33629ada78
oy <roy.legziel@gmail.com>
2020-05-29 10:56:51 +0200

    changed queue time file generation params

--- src/Simulation/Simulation.java
+++ src/Simulation/Simulation.java
@@ -41,7 +41,7 @@ public class Simulation {
     public static ArrayList<Double> corp_times = new ArrayList<Double>();
     public static ArrayList<Double> corp_times_arr = new ArrayList<Double>();
 
-    public Simulation(){
+    public Simulation() {
 
 
         this.list = new CEventList();
@@ -53,8 +53,9 @@ public class Simulation {
 
         // starting agents:
         this.corpAgent1 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 1 agent");
-        this.corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 2 agent");
-        this.corpAgent3 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 3 agent");
+//        this.corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 2 agent");
+//        this.corpAgent3 = new CSACorporate(list, consumer, corp, sink, "shift 1 corp 3 agent");
+
 
 
         this.consAgent1 = new CSAConsumer(list, consumer, sink, "shift 1 cons 1 agent");
@@ -71,7 +72,7 @@ public class Simulation {
     public static void resetAgents() {
         corpAgent1.setActive(false);
         corpAgent2.setActive(false);
-        corpAgent3.setActive(false);
+//        corpAgent3.setActive(false);
 
 
         consAgent1.setActive(false);
@@ -81,15 +82,13 @@ public class Simulation {
     }
 
 
-	
-
-        /**
+    /**
      * @param args the command line arguments
      */
     public static void main(String[] args) throws IOException {
 
-        Simulation sim  = new Simulation();
-        sim.list.start(24*3600+1500);
+        Simulation sim = new Simulation();
+        sim.list.start(24 * 3600 + 1500);
 
 
         String[] ev = sim.sink.getEvents();
@@ -101,28 +100,27 @@ public class Simulation {
             System.out.println(ev[i]);
             System.out.println(stats[i]);
             System.out.println(tim[i]);
-            if (stats[i].contains("Source consumer")){
-                if (i+2 < tim.length) {
-                    consumers_times.add(tim[i + 2] - tim[i]);
+            if (stats[i].contains("Source consumer")) {
+                if (i + 1 < tim.length) {
+                    consumers_times.add(tim[i + 1] - tim[i]);
                     consumers_times_arr.add(tim[i]);
                 }
-            } else if (stats[i].contains("Source corporate")){
-                if (i+2 < tim.length) {
-                    corp_times.add(tim[i + 2] - tim[i]);
+            } else if (stats[i].contains("Source corporate")) {
+                if (i + 1 < tim.length) {
+                    corp_times.add(tim[i + 1] - tim[i]);
                     corp_times_arr.add(tim[i]);
                 }
             }
 
 
         }
-        System.out.println(ev.length/3);
+        System.out.println(ev.length / 3);
         File cons_file = new File("consumers.csv");
         FileWriter cons_fw = new FileWriter(cons_file);
         BufferedWriter cons_bw = new BufferedWriter(cons_fw);
 
-        for(int i=0;i<consumers_times.size();i++)
-        {
-            cons_bw.write(String.valueOf(consumers_times.get(i)/60));
+        for (int i = 0; i < consumers_times.size(); i++) {
+            cons_bw.write(String.valueOf(consumers_times.get(i) / 60));
             cons_bw.newLine();
         }
 
@@ -133,9 +131,8 @@ public class Simulation {
         FileWriter corp_fw = new FileWriter(corp_file);
         BufferedWriter corp_bw = new BufferedWriter(corp_fw);
 
-        for(int i=0;i<corp_times.size();i++)
-        {
-            corp_bw.write(String.valueOf(corp_times.get(i)/60));
+        for (int i = 0; i < corp_times.size(); i++) {
+            corp_bw.write(String.valueOf(corp_times.get(i) / 60));
             corp_bw.newLine();
         }
 
@@ -147,9 +144,8 @@ public class Simulation {
         FileWriter cons_fw_arr = new FileWriter(cons_file_arr);
         BufferedWriter cons_bw_arr = new BufferedWriter(cons_fw_arr);
 
-        for(int i=0;i<consumers_times_arr.size();i++)
-        {
-            cons_bw_arr.write(String.valueOf(consumers_times_arr.get(i)/60));
+        for (int i = 0; i < consumers_times_arr.size(); i++) {
+            cons_bw_arr.write(String.valueOf(consumers_times_arr.get(i) / 60));
             cons_bw_arr.newLine();
         }
 
@@ -160,52 +156,47 @@ public class Simulation {
         FileWriter corp_fw_arr = new FileWriter(corp_file_arr);
         BufferedWriter corp_bw_arr = new BufferedWriter(corp_fw_arr);
 
-        for(int i=0;i<corp_times_arr.size();i++)
-        {
-            corp_bw_arr.write(String.valueOf(corp_times_arr.get(i)/60));
+        for (int i = 0; i < corp_times_arr.size(); i++) {
+            corp_bw_arr.write(String.valueOf(corp_times_arr.get(i) / 60));
             corp_bw_arr.newLine();
         }
 
         corp_bw_arr.close();
         corp_fw_arr.close();
-        }
+    }
 
 
-
-    public static void rosterChange(int i){
-        if(i==0){
-            resetAgents();
+    public static void rosterChange(int i) {
+        if (i == 0) {
+//            resetAgents();
             corpAgent1 = new CSACorporate(list, consumer, corp, sink, "shift 2 corp agent 1");
             corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 2 corp agent 2");
-            corpAgent3 = new CSACorporate(list, consumer, corp, sink, "shift 2 corp agent 3");
 
             consAgent1 = new CSAConsumer(list, consumer, sink, " shift 2 cons agent 1");
             consAgent2 = new CSAConsumer(list, consumer, sink, " shift 2 cons agent 2");
+            consAgent3 = new CSAConsumer(list, consumer, sink, " shift 2 cons agent 3");
 
         } else if (i == 1) {
             resetAgents();
             corpAgent1 = new CSACorporate(list, consumer, corp, sink, "shift 3 corp agent 1");
-            corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 3 corp agent 2");
-
 
             consAgent1 = new CSAConsumer(list, consumer, sink, " shift 3 cons agent 1");
             consAgent2 = new CSAConsumer(list, consumer, sink, " shift 3 cons agent 2");
+            consAgent3 = new CSAConsumer(list, consumer, sink, " shift 3 cons agent 3");
 
         } else if (i == 2) {
             resetAgents();
             corpAgent1 = new CSACorporate(list, consumer, corp, sink, "shift 4 corp agent 1");
-            corpAgent2 = new CSACorporate(list, consumer, corp, sink, "shift 4 corp agent 2");
 
             consAgent1 = new CSAConsumer(list, consumer, sink, " shift 4 cons agent 1");
             consAgent2 = new CSAConsumer(list, consumer, sink, " shift 4 cons agent 2");
+            consAgent3 = new CSAConsumer(list, consumer, sink, " shift 4 cons agent 3");
 
 
         }
 
 
     }
-
-
 }
 
 
